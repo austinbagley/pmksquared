f4d21e44eaa28c307569291eadc08d2659c1c3fa
Austin Bagley <austin.bagley@gmail.com>
2024-06-05 13:22:37 -0600

    added pmk logo

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -1,11 +1,16 @@
 import { CMS_NAME } from "@/lib/constants";
+import Image from 'next/image';
 
 export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
-      </h1>
+      
+      <Image
+          src="/assets/www/pmk-squared-mark.png"
+          alt={``}
+          width={250}
+          height={120}
+        />
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
         A statically generated blog example using{" "}
         <a
